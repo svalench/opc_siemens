e2b0316e72d9ae2357d37be904fb4d8ef74e324f
svalench <chitsalex@gmail.com>
2021-02-10 13:08:11 +0300

    for teldafax

--- data.py
+++ data.py
@@ -18,6 +18,21 @@ list_data1 = [
     {"name": "pol2", "start": 40, "type": "double"}
 ]
 
+list_data2 = [
+    {"name": "vibro1", "start": 0, "type": "int"},
+    {"name": "vibro2", "start": 2, "type": "int"},
+    {"name": "vibro3", "start": 4, "type": "int"},
+    {"name": "vibro4", "start": 6, "type": "int"},
+    {"name": "vibro5", "start": 8, "type": "int"},
+    {"name": "vibro6", "start": 10, "type": "int"},
+    {"name": "temp1", "start": 12, "type": "int"},
+    {"name": "temp2", "start": 14, "type": "int"},
+    {"name": "temp3", "start": 16, "type": "int"},
+    {"name": "temp4", "start": 18, "type": "int"},
+    {"name": "temp5", "start": 20, "type": "int"},
+    {"name": "temp6", "start": 22, "type": "int"},
+]
+
 list_connections = [
     {
         "name": "connect1",
@@ -31,6 +46,16 @@ list_connections = [
         "offset": 168,
         #"offset":44,
         "value_list": list_data
+    },
+{
+        "name": "plc1200_speed_data",
+        "ip": '192.168.32.81',
+        "rack": 0,
+        "slot": 1,
+        'DB': 14,
+        "start": 0,
+        "offset": 24,
+        "value_list": list_data2
     }
 ]
 
