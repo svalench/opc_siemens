8a6c63d9431c73ade9d63dcd90223ba061bcc51e
svalench <chitsalex@gmail.com>
2021-02-15 13:36:16 +0300

    add ststus connection into web app

--- web/app.py
+++ web/app.py
@@ -1,15 +1,19 @@
 from cprint import cprint
 from flask import Flask, render_template
 
+from data import list_connections
+
 app = Flask('opc', static_url_path='', static_folder='web/static', template_folder='web/template')
-connections = None
+connections = []
 
 @app.route('/')
 def index():
     cprint.err("----------------THIS IS STATUSES----------")
-    for i in connections:
-        print(i)
-    return render_template('index.html')
+    data = []
+    for k,i in enumerate(list_connections):
+        i['status_connection'] = connections[k]
+        data.append(i)
+    return render_template('index.html', data=data)
 
 
 
