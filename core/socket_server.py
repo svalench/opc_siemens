136a49f3d50f39d30de4f1557e4fdb27eed28845
svalench <chitsalex@gmail.com>
2021-02-23 09:05:00 +0300

    server socket sendall

--- core/socket_server.py
+++ core/socket_server.py
@@ -86,7 +86,7 @@ def listen_server_mvlab():
                                 start = i * 4096
                                 end = (i + 1) * 4096
                                 conn.send(data[start:end])
-                                time.sleep(0.4)
+                                time.sleep(1)
                                 print(data[start:end])
 
                             conn.close()
