e986390244f269340c5eb5a6c8054f8f941d4e65
svalench <chitsalex@gmail.com>
2021-02-23 10:32:54 +0300

    server socket sendall

--- core/socket_server.py
+++ core/socket_server.py
@@ -82,6 +82,7 @@ def listen_server_mvlab():
                             if "connection_name" in data:
                                 ss = list_connections[data["connection_name"]]['value_list']
                                 ss_len = int(math.ceil((len(ss)/1024) + 1))
+                                ss = json.dumps(ss).encode('utf-8')
                                 conn.send(ss_len.to_bytes(2, 'big'))
                                 for i in range(ss_len):
                                     start = i*ss_len
