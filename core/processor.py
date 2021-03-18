2d5d2f0215a558ecd926060c82339e9dde32738f
svalench <chitsalex@gmail.com>
2021-03-18 16:48:37 +0300

    alarms continue

--- core/processor.py
+++ core/processor.py
@@ -239,7 +239,7 @@ class StartProcessOpcForConnectToPLC(Process):
                         """SELECT COUNT(*) FROM mvlab_alarms  WHERE status=1 and text_alarm = '""" + str(a['text']) + """' and \
                          type_alarm='""" + str(a['type']) + """' and  object_alarm='""" + str(d['name']) + """';""")
                     records = self._c.fetchall()
-                    print(records)
+
                 except:
                     self._c.execute(
                         '''INSERT INTO mvlab_alarms''' \
@@ -249,6 +249,7 @@ class StartProcessOpcForConnectToPLC(Process):
                     self._conn.commit()
                     return False
                 try:
+                    print(len(records))
                     if len(records)!=0:
                         pass
                     else:
