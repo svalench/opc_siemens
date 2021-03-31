c600ec8b009d734b78ed688524da0acc6e65fe39
svalench <chitsalex@gmail.com>
2021-03-31 15:31:25 +0300

    zaebali

--- core/teldafax_dashboard_data.py
+++ core/teldafax_dashboard_data.py
@@ -163,13 +163,14 @@ class PlcRemoteUse():
                 power3 = 0.0
             if power4 >6400:
                 power4 = 0.0
-                cur = createConnection()
-                _c = cur.cursor()
+                _conn = createConnection()
+                _c = _conn.cursor()
             sum_power = power1 + power2 + power3 + power4
             _c.execute(
-                '''INSERT INTO mvlab_s300_db300_power_sum_all''' \
-                """ (value) VALUES ('""" + str(sum_power) + """');""")
-            cur.commit()
+                '''INSERT INTO mvlab_s300_db300_power_sum_all (value) VALUES (''' + str(
+                    sum_power) + ''');''')
+            _conn.commit()
+            _conn.close()
             powers = {"power1": power1, 'power2': power2, 'power3': power3, 'power4': power4, 'sum_power': sum_power}
             return powers
         except:
